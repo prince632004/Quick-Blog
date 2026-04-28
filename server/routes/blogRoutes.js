4b4d74da3249ca78b6cfc32b1d7ae67272ecce0e
Prince Kumar <princenwd2004@gmail.com>
2026-04-28 13:23:14 +0530

    project completed

---
+++ server/routes/blogRoutes.js
@@ -0,0 +1,28 @@
+import express from "express";
+import {
+  addBlog,
+  addComment,
+  deleteBlogById,
+  generateContent,
+  getAllblogs,
+  getBlogById,
+  getBlogComments,
+  togglePublish,
+} from "../controllers/blogControllers.js";
+import upload from "../middlewares/multer.js";
+import auth from "../middlewares/auth.js";
+
+const blogRouter = express.Router();
+
+blogRouter.post("/add", upload.single("image"), auth, addBlog);
+blogRouter.get("/all", getAllblogs);
+blogRouter.get("/:blogId", getBlogById);
+blogRouter.post("/delete", deleteBlogById);
+blogRouter.post("/toggle-publish", auth, togglePublish);
+
+blogRouter.post("/add-comment", addComment);
+blogRouter.post("/comments", getBlogComments);
+
+blogRouter.post("/generate", auth, generateContent);
+
+export default blogRouter;
---
+++ server/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import connectDb from "./configs/db.js";
+import adminRouter from "./routes/adminRoutes.js";
+import blogRouter from "./routes/blogRoutes.js";
+
+const app = express();
+
+await connectDb();
+
+//Middlewares
+app.use(cors());
+app.use(express.json());
+
+//Routes
+app.get("/", (req, res) => res.send("API is working"));
+app.use("/api/admin", adminRouter);
+app.use("/api/blog", blogRouter);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("server is running on port" + PORT);
+});
+
+export default app;
